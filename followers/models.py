638ef06200342cf187b59f843fda91d71b427d16
Lucas Araujo <lucas984618740@gmail.com>
2023-05-04 13:45:59 -0300

    feat: create Followers and like

--- followers/models.py
+++ followers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Follower(models.Model):
+    followed = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="Im_following"
+    )
+    follower = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="my_followers"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
